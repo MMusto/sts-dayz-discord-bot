e6a4c09cd546cff1755fa4e9e3f5a540a1ac9b65
Justin Tran <MMusto@users.noreply.github.com>
2020-01-20 21:13:08 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -262,12 +262,19 @@ async def join(ctx, person = None):
 		if not person:
 			person = 'there'
 		
-		embed = discord.Embed(title="How to Join Our Server", description="Hey {}! Here's how you join.".format(person), inline = False, color=0x09dee1)
-		embed.add_field(name = "Step 1", value = "Download DZSALauncher: https://dayzsalauncher.com/#/home")
-		embed.add_field(name = "Step 2", value = "Search 'Smurf Team Six' in the FILTERS box, located in the TOP LEFT.)")
-		embed.add_field(name = "Step 3", value = "Press the TRIANGLE PLAY button, and DZSALauncher will download ALL the required mods.")
-		embed.add_field(name = "Step 4", value = "Read the #📚server-rules  tab.")
-		embed.add_field(name = "Step 5", value = "Check out the important keybindings here: #💭server-info ")
+		rules = get_channel(str, "rules")
+		if rules:
+			rules = rules.mention
+		info  = get_channel(str, "info")
+		if info:
+			info = info.mention
+		
+		embed = discord.Embed(title="How to Join Our Server", description="Hey {}! Here's how you join.".format(person), inline = True, color=0x09dee1)
+		embed.add_field(name = "Step 1", value = "Download DZSALauncher: https://dayzsalauncher.com/#/home", inline = False)
+		embed.add_field(name = "Step 2", value = "Search 'Smurf Team Six' in the FILTERS box, located in the TOP LEFT.)", inline = False)
+		embed.add_field(name = "Step 3", value = "Press the TRIANGLE PLAY button, and DZSALauncher will download ALL the required mods.", inline = False)
+		embed.add_field(name = "Step 4", value = "Read the {}  tab.".format(rules), inline = False)
+		embed.add_field(name = "Step 5", value = "Check out the important keybindings here: {}".format(info), inline = False)
 		
 		await ctx.send(embed = embed)
 	else:
